253f0d8a0993995003d39bf968ffd67897094a6b
Tristan Le Guern <tleguern@bouledef.eu>
2015-01-04 10:14:38 +0100

    Add a test for overflow at value 65536 in a cell

---
+++ regress/65536.res
@@ -0,0 +1,3 @@
+Number of cells used: 3/1000
+Number of instructions: 122813
+State of the tape: 1 65536 0
--- regress/regress.sh
+++ regress/regress.sh
@@ -51,6 +51,7 @@ t 0 'doubleloop.res' 'doubleloop.b'
 t 0 '50cells.res' '50cells.b'
 t 0 'hello.res' 'hello.b'
 t 1 '/dev/null' 'cristofd-close.b'
+[ "$FULL" -eq 1 ] && t 0 '65536.res' '65536.b'
 
 echo ""
 echo "Failed: $FAILED"
