923095bc31ea6f644e752d0a946f2e9fdf7d33b5
Tristan Le Guern <tleguern@bouledef.eu>
2015-01-18 00:39:19 +0100

    kbf.sh is now sourceable and usable as a library
    kbf detects if it is used directly or is sourced. In the last case
    it doesn't parse its arguments, nor it initialize the instrcutions array and
    the tape.

--- kbf.sh
+++ kbf.sh
@@ -17,61 +17,6 @@ Oflag=0
 Dflag=0
 file=''
 
-while getopts ":c:dst:O:D" opt;do
-	case $opt in
-		c) cflag=$OPTARG;;
-		d) dflag=1;;
-		s) sflag=1;;
-		t) tflag=$OPTARG;;
-		O) Oflag=$OPTARG;;
-		D) Dflag=1;;
-		:) echo "$PROGNAME: option requires an argument -- $OPTARG" >&2;
-		   usage; exit 1;;	# NOTREACHED
-		\?) echo "$PROGNAME: unkown option -- $OPTARG" >&2;
-		   usage; exit 1;;	# NOTREACHED
-		*) usage; exit 1;;	# NOTREACHED
-	esac
-done
-shift $(( $OPTIND -1 ))
-
-if [ -z "$1" ]; then
-	echo "$PROGNAME: file expected" >&2
-	usage
-	exit 1
-else
-	file="$1"
-	shift
-fi
-
-if [ $# -ge 1 ]; then
-	echo "$PROGNAME: invalid trailing chars -- $@" >&2
-	usage
-	exit 1
-fi
-
-set -u
-
-if [ $tflag -le 0 ]; then
-	echo "$PROGNAME: tape size is invalid" >&2
-	exit 1
-fi
-if [ $Oflag -lt 0 ] || [ $Oflag -gt 2 ]; then
-	echo "$PROGNAME: unsupported optimization level - $Oflag" >&2
-	exit 1
-fi
-if ! [ -e "$file" ]; then
-	echo "$PROGNAME: no such file $file" >&2
-	exit 1
-fi
-if ! [ -f "$file" ]; then
-	echo "$PROGNAME: invalid file $file" >&2
-	exit 1
-fi
-if ! [ -r "$file" ]; then
-	echo "$PROGNAME: can't read $file" >&2
-	exit 1
-fi
-
 arrayksh() {
 	local _array_name="$1"
 	shift
@@ -242,68 +187,132 @@ opti2() {
 	fi
 }
 
-set +u
-if [ -n "$BASH_VERSION" ]; then
-	array=arraybash
-elif [ -n "$KSH_VERSION" ]; then
-	array=arrayksh
-elif [ -n "$ZSH_VERSION" ]; then
-	setopt ksharrays
-	array=arrayzsh
-else
-	echo "Error: unsuported shell :(" >&2
-	exit 1
-fi
-set -u
-
-$array i $(cat $file | opti1 | sed 's/./& /g' | opti2)
-$array tape 0
-tptr=0
-ic=0
-iptr=0
-cc=0
-
-case "$cflag" in
-	8) cell=cell8;;
-	16) cell=cell16;;
-	32) cell=cell32;;
-	*) echo "$PROGNAME: Unsupported cell size - $cflag"
-	   exit 1;;
-esac
-
-[ $Dflag -eq 1 ] && echo ${i[*]} && exit 0
-
-trap stats USR1
-
-while [ $iptr -lt ${#i[*]} ]; do
-	jump=0
-	case ${i[$iptr]} in
-		'>') move +1;;
-		'<') move -1;;
-		'+') $cell +1;;
-		'-') $cell -1;;
-		'[') if [ ${tape[$tptr]} -eq 0 ]; then
-			jump=$((`matchingbrace '['` + 1))
-		     fi;;
-		']') if [ ${tape[$tptr]} -ne 0 ]; then
-			jump=`matchingbrace ']'`
-		     fi;;
-		'.') output;;
-		'0') $cell 0;;	# IR operand created by opti2
-		',') input;;
-		*) cc=$(( $cc + 1 ));;
-	esac
-	[ $dflag -eq 1 ] && echo " ${i[$iptr]}: [$tptr]=${tape[$tptr]}" >&2
-
-	ic=$(( $ic + 1 ))
-	if [ $jump -gt 0 ]; then
-		iptr=$jump
+init() {
+	set +u
+	if [ -n "$BASH_VERSION" ]; then
+		array=arraybash
+	elif [ -n "$KSH_VERSION" ]; then
+		array=arrayksh
+	elif [ -n "$ZSH_VERSION" ]; then
+		setopt ksharrays
+		setopt no_function_argzero
+		array=arrayzsh
 	else
-		iptr=$(( $iptr + 1 ))
+		echo "Error: unsuported shell :(" >&2
+		exit 1
 	fi
-done
+	set -u
 
-if [ $sflag -eq 1 ]; then
-	stats
+	case "$cflag" in
+		8) cell=cell8;;
+		16) cell=cell16;;
+		32) cell=cell32;;
+		*) echo "$PROGNAME: Unsupported cell size - $cflag"
+		   exit 1;;
+	esac
+}
+
+kbf() {
+	tptr=0
+	ic=0
+	iptr=0
+	cc=0
+
+	[ $Dflag -eq 1 ] && echo ${i[*]} && exit 0
+
+	trap stats USR1
+
+	while [ $iptr -lt ${#i[*]} ]; do
+		jump=0
+		case ${i[$iptr]} in
+			'>') move +1;;
+			'<') move -1;;
+			'+') $cell +1;;
+			'-') $cell -1;;
+			'[') if [ ${tape[$tptr]} -eq 0 ]; then
+				jump=$((`matchingbrace '['` + 1))
+			     fi;;
+			']') if [ ${tape[$tptr]} -ne 0 ]; then
+				jump=`matchingbrace ']'`
+			     fi;;
+			'.') output;;
+			'0') $cell 0;;	# IR operand created by opti2
+			',') input;;
+			*) cc=$(( $cc + 1 ));;
+		esac
+		[ $dflag -eq 1 ] && echo " ${i[$iptr]}: [$tptr]=${tape[$tptr]}" >&2
+
+		ic=$(( $ic + 1 ))
+		if [ $jump -gt 0 ]; then
+			iptr=$jump
+		else
+			iptr=$(( $iptr + 1 ))
+		fi
+	done
+
+	if [ $sflag -eq 1 ]; then
+		stats
+	fi
+}
+
+if [ "${PROGNAME%.sh}" = "kbf" ]; then
+	while getopts ":c:dst:O:D" opt;do
+		case $opt in
+			c) cflag=$OPTARG;;
+			d) dflag=1;;
+			s) sflag=1;;
+			t) tflag=$OPTARG;;
+			O) Oflag=$OPTARG;;
+			D) Dflag=1;;
+			:) echo "$PROGNAME: option requires an argument -- $OPTARG" >&2;
+			   usage; exit 1;;	# NOTREACHED
+			\?) echo "$PROGNAME: unkown option -- $OPTARG" >&2;
+			   usage; exit 1;;	# NOTREACHED
+			*) usage; exit 1;;	# NOTREACHED
+		esac
+	done
+	shift $(( $OPTIND -1 ))
+
+	if [ -z "$1" ]; then
+		echo "$PROGNAME: file expected" >&2
+		usage
+		exit 1
+	else
+		file="$1"
+		shift
+	fi
+
+	if [ $# -ge 1 ]; then
+		echo "$PROGNAME: invalid trailing chars -- $@" >&2
+		usage
+		exit 1
+	fi
+
+	set -u
+
+	if [ $tflag -le 0 ]; then
+		echo "$PROGNAME: tape size is invalid" >&2
+		exit 1
+	fi
+	if [ $Oflag -lt 0 ] || [ $Oflag -gt 2 ]; then
+		echo "$PROGNAME: unsupported optimization level - $Oflag" >&2
+		exit 1
+	fi
+	if ! [ -e "$file" ]; then
+		echo "$PROGNAME: no such file $file" >&2
+		exit 1
+	fi
+	if ! [ -f "$file" ]; then
+		echo "$PROGNAME: invalid file $file" >&2
+		exit 1
+	fi
+	if ! [ -r "$file" ]; then
+		echo "$PROGNAME: can't read $file" >&2
+		exit 1
+	fi
+
+	init
+	$array i $(cat $file | opti1 | sed 's/./& /g' | opti2)
+	$array tape 0
+	kbf
 fi
-
