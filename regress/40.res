c3417b8e3dcfac2973fd657742e5b313abcca164
Tristan Le Guern <tleguern@bouledef.eu>
2015-01-18 02:59:42 +0100

    Remove 40.b from regress/
    40.b offers nothing 15.b doesn't have, so just kill it.

--- regress/40.res
+++
@@ -1,3 +0,0 @@
-Number of cells used: 2/1000
-Number of instructions: 71
-State of the tape: 0 40
--- regress/regress.sh
+++ regress/regress.sh
@@ -45,7 +45,6 @@ failed() {
 
 t 0 'headcom.res' 'headcom.b'
 t 0 '15.res' '15.b'
-t 0 '40.res' '40.b'
 t 0 '256.res' '256.b'
 t 0 'selfsize.res' 'selfsize.b'
 t 0 'doubleloop.res' 'doubleloop.b'
