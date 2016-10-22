153e578a2fe4de7bb2259edcc3f0c11a68798564
Tristan Le Guern <tleguern@bouledef.eu>
2016-10-22 18:58:39 +0200

    cell32? and cell64s now emit TAP.
    On my loongson laptop cell64s crashs both ksh and bash.

--- regress/internals/cell32u.sh
+++ regress/internals/cell32u.sh
@@ -1,68 +1,63 @@
-#!/bin/ksh
+#!/bin/sh
 
+underflow_1() {
+	echo "underflow 1 : 0 - 1"
+	$array tape 0
+	cell32u -1
+	[ ${tape[$tptr]} -eq 4294967295 ]
+}
+
+underflow_2() {
+	echo "underflow 2 : 0 - 1 - 1"
+	$array tape 0
+	cell32u -1
+	cell32u -1
+	[ ${tape[$tptr]} -eq 4294967294 ]
+}
+
+underflow_3() {
+	echo "underflow 3 : 0 - 2"
+	$array tape 0
+	cell32u -2
+	[ ${tape[$tptr]} -eq 4294967294 ]
+}
+
+overflow_1() {
+	echo "overflow 1 : 4294967295 + 1"
+	$array tape 4294967295
+	cell32u 1
+	[ ${tape[$tptr]} -eq 0 ]
+}
+
+overflow_2() {
+	echo "overflow 2 : 4294967295 + 1 + 1"
+	$array tape 4294967295
+	cell32u 1
+	cell32u 1
+	[ ${tape[$tptr]} -eq 1 ]
+}
+
+overflow_3() {
+	echo "overflow 3 : 4294967295 + 2"
+	$array tape 4294967295
+	cell32u 2
+	[ ${tape[$tptr]} -eq 1 ]
+}
+
+n=1
 . ../../kbf.sh as a library
-
 init
 
-echo -n "underflow 1 : 0 - 1 = "
-$array tape 0
-cell32u -1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 4294967295 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
+echo "TAP version 13"
+echo "1..6"
 
-echo -n "underflow 2 : 0 - 1 - 1 = "
-$array tape 0
-cell32u -1
-cell32u -1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 4294967294 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "underflow 3 : 0 - 2 = "
-$array tape 0
-cell32u -2
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 4294967294 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 1 : 4294967295 + 1 = "
-$array tape 4294967295
-cell32u 1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 0 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 2 : 4294967295 + 1 + 1 = "
-$array tape 4294967295
-cell32u 1
-cell32u 1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 1 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 3 : 4294967295 + 2 = "
-$array tape 4294967295
-cell32u 2
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 1 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
+ts="overflow_1 overflow_2 overflow_3 underflow_1 underflow_2 underflow_3"
+for t in $ts; do
+	if title=$(eval $t); then
+		echo "ok $n - $title"
+	else
+		echo "not ok $n - $title"
+	fi
+	n=$((n + 1))
+done
 
--- regress/internals/cell64s.sh
+++ regress/internals/cell64s.sh
@@ -1,68 +1,63 @@
-#!/bin/ksh
+#!/bin/sh
 
+underflow_1() {
+	echo "underflow 1 : 0 - 1"
+	$array tape 0
+	cell32s -1
+	[ ${tape[$tptr]} -eq 9223372036854775807 ]
+}
+
+underflow_2() {
+	echo "underflow 2 : 0 - 1 - 1"
+	$array tape 0
+	cell32s -1
+	cell32s -1
+	[ ${tape[$tptr]} -eq 9223372036854775806 ]
+}
+
+underflow_3() {
+	echo "underflow 3 : 0 - 2"
+	$array tape 0
+	cell32s -2
+	[ ${tape[$tptr]} -eq 9223372036854775806 ]
+}
+
+overflow_1() {
+	echo "overflow 1 : 9223372036854775807 + 1"
+	$array tape 9223372036854775807
+	cell32s 1
+	[ ${tape[$tptr]} -eq 0 ]
+}
+
+overflow_2() {
+	echo "overflow 2 : 9223372036854775807 + 1 + 1"
+	$array tape 9223372036854775807
+	cell32s 1
+	cell32s 1
+	[ ${tape[$tptr]} -eq 1 ]
+}
+
+overflow_3() {
+	echo "overflow 3 : 9223372036854775807 + 2"
+	$array tape 9223372036854775807
+	cell32s 2
+	[ ${tape[$tptr]} -eq 1 ]
+}
+
+n=1
 . ../../kbf.sh as a library
-
 init
 
-echo -n "underflow 1 : 0 - 1 = "
-$array tape 0
-cell64s -1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 9223372036854775807 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
+echo "TAP version 13"
+echo "1..6"
 
-echo -n "underflow 2 : 0 - 1 - 1 = "
-$array tape 0
-cell64s -1
-cell64s -1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 9223372036854775806 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "underflow 3 : 0 - 2 = "
-$array tape 0
-cell64s -2
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 9223372036854775806 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 1 : 9223372036854775807 + 1 = "
-$array tape 9223372036854775807
-cell64s 1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 0 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 2 : 9223372036854775807 + 1 + 1 = "
-$array tape 9223372036854775807
-cell64s 1
-cell64s 1
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 1 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 3 : 9223372036854775807 + 2 = "
-$array tape 9223372036854775807
-cell64s 2
-echo -n "${tape[$tptr]} -> "
-if [ ${tape[$tptr]} -eq 1 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
+ts="overflow_1 overflow_2 overflow_3 underflow_1 underflow_2 underflow_3"
+for t in $ts; do
+	if title=$(eval $t); then
+		echo "ok $n - $title"
+	else
+		echo "not ok $n - $title"
+	fi
+	n=$((n + 1))
+done
 
