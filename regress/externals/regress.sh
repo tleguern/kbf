b10baa3a274a644635c460f48c1246f0cd51dfe1
Tristan Le Guern <tleguern@bouledef.eu>
2016-10-23 18:55:26 +0200

    The "externals" regress tests now emits TAP, too!

--- regress/externals/regress.sh
+++ regress/externals/regress.sh
@@ -2,8 +2,7 @@
 # Tristan Le Guern <tleguern@bouledef.eu>
 # Public domain
 
-FAILED=0
-
+n=0
 kbf=${kbf:-../../kbf.sh}
 FULL=${FULL:-0}
 
@@ -12,16 +11,17 @@ t() {
 	# $2 -> file containing the expected result
 	# $3 -> $test expression
 	# $4 -> Options to kbf
-	echo "Run $kbf $4 \"$3\", expect exit code $1"
 
-	tmp=`mktemp -t kbf.XXXXXXXX`
+	title="$kbf $4 \"$3\", exit code $1"
+	n=$((n + 1))
+
+	tmp=$(mktemp -t kbf.XXXXXXXX)
 	set +e
 	$kbf $4 -s $3 > $tmp 2> /dev/null
 	ret=$?
 	set -e
 	if [ $ret -ne $1 ]; then
-		echo "Wrong exit code for $3 ($ret)"
-		failed
+		echo "not ok $n - $title (wrong exit code $ret)"
 		rm $tmp
 		return
 	fi
@@ -30,19 +30,20 @@ t() {
 	ret=$?
 	set -e
 	if [ $ret -ne $1 ]; then
-		echo "Wrong result for $3 ($ret)"
-		failed
+		echo "not ok $n - $title (wrong result)"
 		rm $tmp
 		return
 	fi
 	rm $tmp
-	echo OK
+	echo "ok $n - $title"
 }
 
-failed() {
-	echo "Failed"
-	FAILED=`expr $FAILED + 1`
-}
+echo "TAP version 13"
+if [ "$FULL" = "0" ]; then
+	echo "1..23"
+else
+	echo "1..25"
+fi
 
 t 0 'headcom.res'	'headcom.b'	'-t 1'
 t 0 '15.res'		'15.b'		'-t 2'
@@ -52,30 +53,24 @@ t 0 'selfsize.res'	'selfsize.b'	'-t 3'
 t 0 'doubleloop.res'	'doubleloop.b'	'-t 3'
 t 0 '50cells.res'	'50cells.b'	'-t 50'
 t 0 'hello.res'		'hello.b'	'-t 7'
-
 t 1 '/dev/null'		'cristofd-close.b'
-
 t 0 'O1.res' 'optimization.b' '-D -O1'
 t 0 'O1.res' 'optimization.b' '-D -o strip-comments'
 t 0 'O2.res' 'optimization.b' '-D -O2'
 t 0 'O2.res' 'optimization.b' '-D -o strip-comments -o strip-empty-and-null'
 t 0 'O3.res' 'optimization.b' '-D -O3'
 t 0 'O3.res' 'optimization.b' '-D -o strip-comments -o strip-empty-and-null -o optimized-operands'
-
 t 0 'prevzero.res'	'prevzero.b'	'-o optimized-operands -t 10'
 t 0 'prevzero_op.res'	'prevzero_op.b'	'-O0 -t 10'
 t 0 'nextzero.res'	'nextzero.b'	'-o optimized-operands -t 10'
 t 0 'nextzero_op.res'	'nextzero_op.b'	'-O0 -t 10'
 t 0 'zero.res'		'zero.b'	'-o optimized-operands -t 2'
 t 0 'zero_op.res'	'zero_op.b'	'-O0 -t 2'
-
 t 0 '15rle.res'		'15.b'	'-O0 -o run-length-encoding -t 2'
 t 0 '256rle.res'	'256.b'	'-O0 -o run-length-encoding -t 2'
 
-[ $FULL -eq 1 ] && t 0 '65536.res' '65536.b' '-t2'
-
-echo ""
-echo "Failed: $FAILED"
-[ $FAILED -gt 0 ] && exit 1
-exit 0
+if [ $FULL -eq 1 ]; then
+	t 0 '65536.res' '65536.b' '-t2'
+	t 0 'bitwidth_O4_c8.res' 'bitwidth.b' '-O4 -c8'
+fi
 
