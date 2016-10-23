e1600b8bdbbbed661845c4c1c8520816aae1674d
Tristan Le Guern <tleguern@bouledef.eu>
2016-10-23 23:40:44 +0200

    Add a short documentation for the new benchmark tool

--- tools/README.md
+++ tools/README.md
@@ -1,9 +1,17 @@
 Tools
 =====
 
+benchmark.sh
+------------
+
+This scripts takes no parameter, it should be ran in a directory
+containing a directory name 'results'. It will creates files containing
+the execution times of special TESTFILE with the given SHELL.
+
 popularity.sh
 -------------
 
 This scripts takes no parameter, it should just be ran in a directory 
 full of brainfuck programs (ie: ending in .b). It uses kbf to dump the 
 intermediary representation and then sort the operators by popularity.
+
--- tools/benchmark.sh
+++ tools/benchmark.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# Tristan Le Guern <tleguern@bouledef.eu> - Public domain
 
-testfile=../regress/externals/bitwidth.b
-shell=ksh
+TESTFILE=${TESTFILE:-../regress/externals/bitwidth.b}
+SHELL=${SHELL:-ksh}
 
 for size in 8 16 24 32s 32u 64s; do
 	for opt in 0 1 2 3 4; do
-		{ time $shell ../kbf.sh -s -c $size -O$opt $testfile; } > results/$(basename ${testfile%%.b})_c${size}_${opt}.$shell 2>&1
+		{ time $SHELL ../kbf.sh -s -c $size -O$opt $TESTFILE; } > results/$(basename ${TESTFILE%%.b})_c${size}_${opt}.$SHELL 2>&1
 	done
 done
 
