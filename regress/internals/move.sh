b1ccfe03245778a6d50c688aaae88ba9f4beeeae
Tristan Le Guern <tleguern@bouledef.eu>
2016-10-27 19:40:17 +0200

    move.sh now emits TAP

--- regress/internals/move.sh
+++ regress/internals/move.sh
@@ -1,38 +1,40 @@
 #!/bin/ksh
 
 . ../../kbf.sh as a library
-
 init
 $array tape 0 1 2 3 4 5 6 7 8 9
 
-echo -n "move 5 cells to the right "
+echo "TAP version 13"
+echo "1..4"
+
+title="move 5 cells to the right"
 move 5
-if [ ${tape[$tptr]} -ne 5 ]; then
-	echo "KO"
+if [ ${tape[$tptr]} -eq 5 ]; then
+	echo "ok 1 - $title"
 else
-	echo "OK"
+	echo "not ok 1 - $title"
 fi
 
-echo -n "move 2 cells to the left "
+title="move 2 cells to the left"
 move -2
-if [ ${tape[$tptr]} -ne 3 ]; then
-	echo "KO"
+if [ ${tape[$tptr]} -eq 3 ]; then
+	echo "ok 2 - $title"
 else
-	echo "OK"
+	echo "not ok 2 - $title"
 fi
 
-echo -n "move 3 cells to the left "
+title="move 3 cells to the left"
 move -3
-if [ ${tape[$tptr]} -ne 0 ]; then
-	echo "KO"
+if [ ${tape[$tptr]} -eq 0 ]; then
+	echo "ok 3 - $title"
 else
-	echo "OK"
+	echo "not ok 3 - $title"
 fi
 
-echo -n "move 1 cell bellow zero "
-if ! $(move -1 > /dev/null 2>&1); then
-	echo "not possible - OK"
+title="move bellow zero (this should fail !)"
+if $(move -1 2> /dev/null); then
+	echo "not ok 4 - $title"
 else
-	echo "possible - KO"
+	echo "ok 4 - $title"
 fi
 
