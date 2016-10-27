f5e62d3df440ce7a463cc081d873a7eb8f3a8ae4
Tristan Le Guern <tleguern@bouledef.eu>
2016-10-27 23:55:42 +0200

    cell8 now emits TAP.
    I completely forgotten this one.

--- regress/internals/cell8.sh
+++ regress/internals/cell8.sh
@@ -1,68 +1,63 @@
-#!/bin/ksh
+#!/bin/sh
 
+underflow_1() {
+	echo "underflow 1 : 0 - 1"
+	$array tape 0
+	cell8 -1
+	[ ${tape[$tapeidx]} -eq 255 ]
+}
+
+underflow_2() {
+	echo "underflow 2 : 0 - 1 - 1"
+	$array tape 0
+	cell8 -1
+	cell8 -1
+	[ ${tape[$tapeidx]} -eq 254 ]
+}
+
+underflow_3() {
+	echo "underflow 3 : 0 - 2"
+	$array tape 0
+	cell8 -2
+	[ ${tape[$tapeidx]} -eq 254 ]
+}
+
+overflow_1() {
+	echo "overflow 1 : 255 + 1"
+	$array tape 255
+	cell8 1
+	[ ${tape[$tapeidx]} -eq 0 ]
+}
+
+overflow_2() {
+	echo "overflow 2 : 255 + 1 + 1"
+	$array tape 255
+	cell8 1
+	cell8 1
+	[ ${tape[$tapeidx]} -eq 1 ]
+}
+
+overflow_3() {
+	echo "overflow 3 : 255 + 2"
+	$array tape 255
+	cell8 2
+	[ ${tape[$tapeidx]} -eq 1 ]
+}
+
+n=1
 . ../../kbf.sh as a library
-
 init
 
-echo -n "underflow 1 : 0 - 1 = "
-$array tape 0
-cell8 -1
-echo -n "${tape[$tapeidx]} -> "
-if [ ${tape[$tapeidx]} -eq 255 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
+echo "TAP version 13"
+echo "1..6"
 
-echo -n "underflow 2 : 0 - 1 - 1 = "
-$array tape 0
-cell8 -1
-cell8 -1
-echo -n "${tape[$tapeidx]} -> "
-if [ ${tape[$tapeidx]} -eq 254 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "underflow 3 : 0 - 2 = "
-$array tape 0
-cell8 -2
-echo -n "${tape[$tapeidx]} -> "
-if [ ${tape[$tapeidx]} -eq 254 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 1 : 255 + 1 = "
-$array tape 255
-cell8 1
-echo -n "${tape[$tapeidx]} -> "
-if [ ${tape[$tapeidx]} -eq 0 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 2 : 255 + 1 + 1 = "
-$array tape 255
-cell8 1
-cell8 1
-echo -n "${tape[$tapeidx]} -> "
-if [ ${tape[$tapeidx]} -eq 1 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
-
-echo -n "overflow 3 : 255 + 2 = "
-$array tape 255
-cell8 2
-echo -n "${tape[$tapeidx]} -> "
-if [ ${tape[$tapeidx]} -eq 1 ]; then
-	echo "OK"
-else
-	echo "KO"
-fi
+ts="overflow_1 overflow_2 overflow_3 underflow_1 underflow_2 underflow_3"
+for t in $ts; do
+	if title=$(eval $t); then
+		echo "ok $n - $title"
+	else
+		echo "not ok $n - $title"
+	fi
+	n=$((n + 1))
+done
 
