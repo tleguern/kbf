d0ebd4d484cd3b9576a5f5e1febd69cf68556864
Tristan Le Guern <tleguern@bouledef.eu>
2018-11-21 22:28:13 +0000

    Really test cell64s in cell64s.sh

--- regress/internals/cell64s.sh
+++ regress/internals/cell64s.sh
@@ -3,44 +3,44 @@
 underflow_1() {
 	echo "underflow 1 : 0 - 1"
 	$array tape 0
-	cell32s -1
+	cell64s -1
 	[ ${tape[$tapeidx]} -eq 9223372036854775807 ]
 }
 
 underflow_2() {
 	echo "underflow 2 : 0 - 1 - 1"
 	$array tape 0
-	cell32s -1
-	cell32s -1
+	cell64s -1
+	cell64s -1
 	[ ${tape[$tapeidx]} -eq 9223372036854775806 ]
 }
 
 underflow_3() {
 	echo "underflow 3 : 0 - 2"
 	$array tape 0
-	cell32s -2
+	cell64s -2
 	[ ${tape[$tapeidx]} -eq 9223372036854775806 ]
 }
 
 overflow_1() {
 	echo "overflow 1 : 9223372036854775807 + 1"
 	$array tape 9223372036854775807
-	cell32s 1
+	cell64s 1
 	[ ${tape[$tapeidx]} -eq 0 ]
 }
 
 overflow_2() {
 	echo "overflow 2 : 9223372036854775807 + 1 + 1"
 	$array tape 9223372036854775807
-	cell32s 1
-	cell32s 1
+	cell64s 1
+	cell64s 1
 	[ ${tape[$tapeidx]} -eq 1 ]
 }
 
 overflow_3() {
 	echo "overflow 3 : 9223372036854775807 + 2"
 	$array tape 9223372036854775807
-	cell32s 2
+	cell64s 2
 	[ ${tape[$tapeidx]} -eq 1 ]
 }
 
